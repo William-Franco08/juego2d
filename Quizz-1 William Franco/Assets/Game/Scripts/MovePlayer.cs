327032125faa7387b6f03ccbb9f575489ce374f7
William-Franco08 <flw5856@gmail.com>
2026-03-20 15:14:14 -0500

    salto-animacion

--- Quizz-1 William Franco/Assets/Game/Scripts/MovePlayer.cs
+++ Quizz-1 William Franco/Assets/Game/Scripts/MovePlayer.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class MovePlayer : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 10f;
+
 
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    private bool isGrounded = true;
+    public Animator animator;
 
     private void Awake()
     {
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -19,8 +25,50 @@ public class MovePlayer : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
     }
 
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            isGrounded = false;
+        }
+    }
+
     private void FixedUpdate()
     {
+
+        animator.SetFloat("Speed", Mathf.Abs(moveInput.x));
+        animator.SetBool("isGrounded", isGrounded);
+        animator.SetFloat("yVelocity", rb.linearVelocity.y);
         rb.linearVelocity = new Vector2(moveInput.x * speed, rb.linearVelocity.y);
+        Vector3 scale = transform.localScale;
+
+        if (moveInput.x > 0)
+            scale.x = Mathf.Abs(scale.x);
+        else if (moveInput.x < 0)
+            scale.x = -Mathf.Abs(scale.x);
+
+        transform.localScale = scale;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            foreach (ContactPoint2D contact in collision.contacts)
+            {
+                if (contact.normal.y > 0.5f)
+                {
+                    isGrounded = true;
+                }
+            }
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
     }
 }
